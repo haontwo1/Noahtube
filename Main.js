6cd533e9b7c92fc23a3e23b12fb9389df3cfcb0a
haontwo1 <haoneturt@gmail.com>
2025-04-25 22:20:54 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,6 @@
-document.body.innerHTML = ' ';
+if (window.location.hostname === "edpuzzle.com") {
+  window.location.href = "https://www.youtube.com";
+} else {
+  alert("This code only works on \"edpuzzle.com\"");
+}
+
