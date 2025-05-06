004830e936f6258904fc6a97b4c22cf9514b33b8
haontwo1 <haoneturt@gmail.com>
2025-05-06 17:23:13 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,12 +1,4 @@
-fetch("https://api.sheetbest.com/sheets/45296d18-ffb5-4821-9c29-eff13a3d4327", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    used_at: new Date().toISOString()
-  })
-});
+
 if (window.location.hostname === "edpuzzle.com") {
   window.location.href = "https://www.youtube.com";
 } else {
